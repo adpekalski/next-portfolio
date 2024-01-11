60f04e62c90ffa0c50407f281afe7e4625c9b4ed
A. Pękalski <adpekalski@gmail.com>
2024-01-11 19:05:12 +0100

    Change project component
    *Change project to use different description from data based on current device
    *Project title based on data
    *Show "Readm more" and "Go back" on smaller devices

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -7,55 +7,43 @@ import { projectsData } from '@/data/ProjectsData';
 
 import Card from '../card/Card';
 
+
+
 const Project = (props) => {
-    const { which } = props;
+    const { which, usedHeight, device } = props;
 
     const [hidden, setHidden] = React.useState(false);
-    let isHidden = hidden ? "display-none" : " ";
+    let showMoreOrLess = hidden ? "Go back" : "Read more";
 
     let currentProject = projectsData.description[which];
+    let choosenCard = <Card picture={which}></Card>;
+    if (device === "mobile" || usedHeight < 800) {
+        currentProject = (
+            <>
+                {hidden ? projectsData.description[which] + " " : projectsData.shortDescription[which] + " "}
+
+                <span className='expand-text'>{showMoreOrLess}</span>
+            </>
+        );
+        choosenCard = hidden ? null : <Card picture={which}></Card>;
+
+    }
     return (
         <div className='project'>
-            <h1 className={second.className + ' project--name'}>Project X</h1>
-            {hidden ? null : <Card></Card>}
+            <h1
+                className={second.className + ' project--name'}
+            >
+                {projectsData.projectName[which]}
+            </h1>
+            {choosenCard}
             <p
                 className='project--desc'
                 onClick={() => {
-                    setHidden(prev => !prev)
-                    console.log(hidden);
+                    setHidden(prev => !prev);
                 }}
             >
                 {currentProject}
-
-                {/* This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun
-                This project is fun */}
             </p>
-            {/* <p className='swipe'>swipe</p> */}
         </div>
     );
 };
