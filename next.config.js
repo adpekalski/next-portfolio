e8a5a5748004181b399471c78bbf7b6c10d994a2
A. Pękalski <adpekalski@gmail.com>
2024-01-11 18:38:12 +0100

    Static HTML export

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+}
 
 module.exports = nextConfig
 
-// module.exports = {
-//     reactStrictMode: true, // was there by default
-//     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-//         config.module.rules.push({
-//             test: /\.(png|jpe?g|gif)$/i,
-//             use: [
-//                 {
-//                     loader: 'file-loader',
-//                     options: {
-//                         publicPath: 'public',
-//                     },
-//                 },
-//             ],
-//         });
-
-//         // Important: return the modified config
-//         return config;
-//     },
-// };
