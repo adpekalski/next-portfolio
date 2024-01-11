2a4e410e5dccdfbb8f1aa1ccbdc3b8129103ef9c
A. Pękalski <adpekalski@gmail.com>
2024-01-11 19:08:45 +0100

    Change about page
    *Added marquee with icons at the bottom of about section
    *Crated custom overlay above the marquee to display it begind a gradient
    *About description using data based on current device

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,26 +5,42 @@ import '@/components/ultimate.css';
 import { second } from '@/app/fonts';
 // import CV from '$/CV.pdf';
 
-const About = () => {
+import { motion } from 'framer-motion';
 
-    
+import Marquee from 'react-fast-marquee';
+
+import { aboutContent } from '@/data/ProjectsData';
+
+const About = (props) => {
+
+    const { device } = props;
+
+    const mobileClass = device === "mobile" ? "" : "display-none";
+
 
     return (
         <section className='about' id='about' tabIndex={0}>
             <div className='about-container'>
 
-
-                <div className='about-sides'>
-                    <i class="devicon-css3-plain"></i>
-                </div>
-
                 <div className='about-center'>
-                    <h1 className={second.className + ' about-me'}>About me</h1>
-                    <p className='something-about'>Some text which is very long as fuck</p>
-                    <div className='mobile-icons'>
-                        <i class="devicon-css3-plain"></i>
-                    </div>
-                    <a href='/CV.pdf' download='Pękalski_Adrian_CV.pdf'>
+                    <h1
+                        className={second.className + ' about-me'}
+                    >About me
+                    </h1>
+                    <p
+                        className='something-about'
+                    >
+                        {
+                            device !== "mobile" ?
+                                aboutContent.aboutText[1]
+                                : aboutContent.shortAboutText[1]
+                        }
+                    </p>
+
+                    <a
+                        href='/CV.pdf'
+                        download='Pękalski_Adrian_CV.pdf'
+                    >
                         <button
                             type='button'
                             className='btn-download primary-btn btn'
@@ -32,19 +48,39 @@ const About = () => {
                             Download CV
                         </button>
                     </a>
-                </div>
 
-
-
-                <div className='about-sides'>
-
-                    <i class="devicon-css3-plain"></i>
 
                 </div>
-
-
+                <div className='about-bottom'>
+                    <Marquee
+                        className='mobile-icons'
+                        speed={50}
+                        autoFill
+                    >
+                        <i className="devicon-css3-plain"></i>
+                        <i className="devicon-html5-plain"></i>
+                        <i className="devicon-nextjs-original"></i>
+                        <i className="devicon-git-plain"></i>
+                        <i className="devicon-github-original"></i>
+                        <i className="devicon-nodejs-plain"></i>
+                        <i className="devicon-javascript-plain"></i>
+                        <i className="devicon-storybook-plain"></i>
+                        <i className="devicon-webpack-plain"></i>
+                        <i className="devicon-babel-plain"></i>
+                        <i className="devicon-typescript-plain"></i>
+                        <i className="devicon-bootstrap-plain"></i>
+                        <i className="devicon-canva-original"></i>
+                        <i className="devicon-figma-plain"></i>
+                        <i className="devicon-heroku-original"></i>
+                        <i className="devicon-mongodb-plain"></i>
+                        <i className="devicon-npm-original-wordmark"></i>
+                        <i className="devicon-react-original"></i>
+                    </Marquee>
+                </div>
             </div>
 
+            <div className='about-bottom-overlay-left'></div>
+            <div className='about-bottom-overlay-right'></div>
 
         </section>
     );
