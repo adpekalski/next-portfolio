cfe69d6989cac17ca94c13a165375744ab1df4f6
A. Pękalski <adpekalski@gmail.com>
2024-01-11 18:48:07 +0100

    New projects data

--- src/data/ProjectsData.jsx
+++ src/data/ProjectsData.jsx
@@ -1,33 +1,50 @@
-import firstProjectScreen from "$/avatar.png";
-import secondProjectScreen from "$/avatar.png";
-import thirdProjectScreen from "$/avatar.png";
+import firstProjectScreen from "$/portfolio.png";
+import secondProjectScreen from "$/coming_soon.png";
+import thirdProjectScreen from "$/coming_soon.png";
+import fourthProjectScreen from "$/coming_soon.png";
 
 export const projectsData = {
     projectImg: {
-        first: firstProjectScreen,
-        second: secondProjectScreen,
-        third: thirdProjectScreen
+        1: firstProjectScreen,
+        2: secondProjectScreen,
+        3: thirdProjectScreen,
+        4: fourthProjectScreen
+    },
+    projectName: {
+        1: "Portfolio",
+        2: "Library",
+        3: "Games",
+        4: "Shop"
     },
     description: {
-        first: "Website developed as a portfolio showcasing the most relevant projects " +
+        1: "Website developed as a portfolio showcasing the most relevant projects " +
             "that I made. Fully interactive, with customization possibilities, such as " +
             "color selection, dark and light modes, and a way to stop typing animation. " +
             "The project includes a functioning contact form implemented using EmailJS, " +
             "so there is a possibility to contact me directly through this website.",
-        second: "loll",
-        third: ""
+        2: "My own React components library.",
+        3: "",
+        4: ""
     },
-    descriptionSecondaryText: {
-        first: "During development, instead of bootstrap, I used some functionalities provided " +
-        "by Mui, such as icons, buttons, and a navigation tab, all of which were customized. " +
-        "LazyLoad was used on images to optimize page load times. Most of the remaining " +
-        " components were written entirely by me.",
-        second: "",
-        third: ""
+    shortDescription: {
+        1: "Website showcasing projects made as a fancy application full of animations.",
+        2: "My own React components library.",
+        3: "Web application which gives opportunity to play some 'brain stretching' games.",
+        4: "Project of a example company webiste with store."
     },
     linkToProject: {
-        first: "",
-        second: "",
-        third: ""
+        1: "",
+        2: "",
+        3: "",
+        4: ""
     }
 };
+
+export const aboutContent = {
+    aboutText: {
+        1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris imperdiet quam sapien, sed mollis urna tincidunt non. In a lorem non elit fringilla hendrerit eget eu odio. Donec et vulputate mi, non vestibulum nulla. Duis imperdiet ligula sit amet tellus iaculis dignissim. Donec hendrerit eget nulla ut tempor. Donec vitae ligula ex. Etiam et dui mi. Praesent suscipit sem enim, ut tristique metus consequat ut. Phasellus risus purus, fermentum at sodales at, blandit eu nisl. Vestibulum eu quam molestie, mollis turpis id, ultrices quam. Ut ullamcorper porta libero, id dapibus velit iaculis eu. Morbi malesuada elementum eleifend. Phasellus magna nunc, rutrum lobortis sem in, tincidunt tristique nisi."
+    },
+    shortAboutText: {
+        1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris imperdiet quam sapien, sed mollis urna tincidunt non. In a lorem non elit fringilla hendrerit eget eu odio. Donec et vulputate mi, non vestibulum nulla. Duis imperdiet ligula sit amet tellus iaculis dignissim. Donec hendrerit eget nulla ut tempor. Donec vitae ligula ex."
+    }
+}
