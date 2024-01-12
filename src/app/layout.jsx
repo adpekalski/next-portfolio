677d8744b2e33169103c77790bb5152438e81a77
A. Pękalski <adpekalski@gmail.com>
2024-01-12 11:22:15 +0100

    Adding favicon for all cases

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
+        <meta name="msapplication-TileColor" content="#140606" />
+        <meta name="theme-color" content="#140606" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0c661b" />
       </head>
       <body className={first.className}>{children}</body>
     </html>
