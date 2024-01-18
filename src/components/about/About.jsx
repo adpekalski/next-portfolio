3687a17d8c4a8bbcba1b25452405c639df43b56f
A. Pękalski <adpekalski@gmail.com>
2024-01-18 02:58:11 +0100

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,82 +9,108 @@ import { motion } from 'framer-motion';
 
 import Marquee from 'react-fast-marquee';
 
-import { aboutContent } from '@/data/ProjectsData';
+import { aboutContent } from '@/data/WebsiteData';
+import Button from '../button/Button';
+import Title from '../title/Title';
 
 const About = (props) => {
+    const { minDeviceForMoreText, minDeviceForSplit } = props;
 
-    const { device } = props;
 
-    const mobileClass = device === "mobile" ? "" : "display-none";
-    const [minHeightMatch, setMatch] = React.useState(false);
-
-    React.useEffect(() => {
-        setMatch(window.matchMedia("(min-height: 900px)").matches);
-    })
 
     return (
         <section className='about' id='about' tabIndex={0}>
             <div className='about-container'>
 
                 <div className='about-center'>
-                    <h1
-                        className={second.className + ' about-me'}
-                    >About me
-                    </h1>
-                    <p
-                        className='something-about'
-                    >
-                        {
-                            minHeightMatch ?
-                                aboutContent.aboutText[1]
-                                : aboutContent.shortAboutText[1]
-                        }
-                    </p>
+
+                    <Title>About me</Title>
+
+                    {minDeviceForMoreText ?
+
+                        (minDeviceForSplit ?
+                            <div className='two-columns'>
+                                <div className='something-about'>
+                                    <span className='green-text'>It's truly exciting to have you here. </span>
+                                    <p>{aboutContent.aboutText[1]}</p>
+                                    <p>{aboutContent.aboutText[2]}</p>
+                                    <p>{aboutContent.aboutText[3]}</p>
+                                </div>
+                                <div className='something-about'>
+                                    <p>{aboutContent.aboutText[4]}</p>
+                                    <p className='green-text'>{aboutContent.aboutText[5]}</p>
+                                    <p>{aboutContent.aboutText[6]}</p>
+                                    <p className='green-text'>{aboutContent.aboutText[7]}</p>
+                                </div>
+                            </div>
+                            :
+                            <div className='something-about'>
+                                <span className='green-text'>It's truly exciting to have you here. </span>
+                                <p>{aboutContent.aboutText[1]}</p>
+                                <p>{aboutContent.aboutText[2]}</p>
+                                <p>{aboutContent.aboutText[3]}</p>
+                                <p>{aboutContent.aboutText[4]}</p>
+                                <p className='green-text'>{aboutContent.aboutText[5]}</p>
+                                <p>{aboutContent.aboutText[6]}</p>
+                                <p className='green-text'>{aboutContent.aboutText[7]}</p>
+                            </div>
+                        )
+                        :
+                        <div className='something-about'>
+                            <p>{aboutContent.shortAboutText[1]}</p>
+                            <p>{aboutContent.shortAboutText[2]}</p>
+                        </div>
+                    }
 
                     <a
+                        className='download-anchor'
                         href='/Pękalski_Adrian_CV.pdf'
                         download
                     >
-                        <button
+                        <Button style="primary">
+                            Download CV
+                        </Button>
+                        {/* <motion.button
+                            tabIndex={-1}
                             type='button'
                             className='btn-download primary-btn btn'
+
                         >
-                            Download CV
-                        </button>
+                            <span>Download CV</span>
+                        </motion.button> */}
                     </a>
 
 
                 </div>
-                <div className='about-bottom'>
-                    <Marquee
-                        className='mobile-icons'
-                        speed={50}
-                        autoFill
-                    >
-                        <i className="devicon-css3-plain"></i>
-                        <i className="devicon-html5-plain"></i>
-                        <i className="devicon-nextjs-original"></i>
-                        <i className="devicon-git-plain"></i>
-                        <i className="devicon-github-original"></i>
-                        <i className="devicon-nodejs-plain"></i>
-                        <i className="devicon-javascript-plain"></i>
-                        <i className="devicon-storybook-plain"></i>
-                        <i className="devicon-webpack-plain"></i>
-                        <i className="devicon-babel-plain"></i>
-                        <i className="devicon-typescript-plain"></i>
-                        <i className="devicon-bootstrap-plain"></i>
-                        <i className="devicon-canva-original"></i>
-                        <i className="devicon-figma-plain"></i>
-                        <i className="devicon-heroku-original"></i>
-                        <i className="devicon-mongodb-plain"></i>
-                        <i className="devicon-npm-original-wordmark"></i>
-                        <i className="devicon-react-original"></i>
-                    </Marquee>
-                </div>
+
             </div>
 
-            <div className='about-bottom-overlay-left'></div>
-            <div className='about-bottom-overlay-right'></div>
+            <div className='about-bottom'>
+                <Marquee
+                    className='mobile-icons'
+                    speed={50}
+                    autoFill
+                >
+                    <i className="devicon-css3-plain"></i>
+                    <i className="devicon-html5-plain"></i>
+                    <i className="devicon-nextjs-original"></i>
+                    <i className="devicon-git-plain"></i>
+                    <i className="devicon-github-original"></i>
+                    <i className="devicon-nodejs-plain"></i>
+                    <i className="devicon-javascript-plain"></i>
+                    <i className="devicon-storybook-plain"></i>
+                    <i className="devicon-webpack-plain"></i>
+                    <i className="devicon-babel-plain"></i>
+                    <i className="devicon-typescript-plain"></i>
+                    <i className="devicon-bootstrap-plain"></i>
+                    <i className="devicon-canva-original"></i>
+                    <i className="devicon-figma-plain"></i>
+                    <i className="devicon-heroku-original"></i>
+                    <i className="devicon-mongodb-plain"></i>
+                    <i className="devicon-npm-original-wordmark"></i>
+                    <i className="devicon-react-original"></i>
+                </Marquee>
+            </div>
 
         </section>
     );
