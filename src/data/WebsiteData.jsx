07e9b5e17b4efe8760e768e09e6103054569d384
A. Pękalski <adpekalski@gmail.com>
2024-01-18 02:52:01 +0100

    Update data and change file name

---
+++ src/data/WebsiteData.jsx
@@ -0,0 +1,61 @@
+import firstProjectScreen from "$/portfolio.png";
+import secondProjectScreen from "$/coming_soon.png";
+import thirdProjectScreen from "$/coming_soon.png";
+import fourthProjectScreen from "$/coming_soon.png";
+
+export const projectsData = {
+    projectImg: {
+        1: firstProjectScreen,
+        2: secondProjectScreen,
+        3: thirdProjectScreen,
+        4: fourthProjectScreen
+    },
+    projectName: {
+        1: "Portfolio",
+        2: "Library",
+        3: "Brain Stretch",
+        4: "E-Commerce"
+    },
+    description: {
+        1: "Portfolio site presenting my projects with engaging animations and a unique style that resonates with my personality. Constructed with Next.js and powered by Framer Motion. Crafted using Swiper, EmailJS, Feather Icons and React Fast Marquee. Incorporating a fully functional contact form equipped with validation.",
+        2: "My own React component library, infused with animations and crafted using Storybook.",
+        3: 'Web application allowing users to play cognitively engaging and "brain stretching" games.',
+        4: "Showcase website representing a company, complete with integrated e-commerce capabilities."
+    },
+    shortDescription: {
+        1: "Portfolio site presenting my projects with engaging animations and a unique style that resonates with my personality.",
+        2: "My own React component library, infused with animations and crafted using Storybook.",
+        3: 'Web application allowing users to play cognitively engaging and "brain stretching" games.',
+        4: "Showcase website representing a company, complete with integrated e-commerce capabilities."
+    },
+    linkToProject: {
+        1: "https://github.com/adpekalski/next-portfolio",
+        2: "",
+        3: "",
+        4: ""
+    }
+};
+
+export const aboutContent = {
+    aboutText: {
+        1: "I'm Adrian, a passionate professional with a distinctive journey that intertwines engineering expertise with a fervor for web development.",
+        2: "With a foundation in Automation Engineering, I have adeptly navigated the complex realms of industry and technology, yet my true excitement has always revolved around crafting exceptional user interfaces.",
+        3: "Eager for more challenges and yearning for the opportunity to specialize in UI, I decided to embark on a self-guided journey to master programming.",
+        4: "Everything you see in my portfolio is a result of this personal exploration. I have dedicated numerous evenings with enthusiasm to unraveling the intricacies of frontend development.",
+        5: "Below you can see some of the skills I've acquired along the way.",
+        6: "Programming has undeniably become my passion, and the prospect of pushing my limits excites me. Deep down, I harbor a dream of crafting my own game someday.",
+        7: "If you're interested in collaboration, don't hesitate to reach out!"
+
+    },
+    shortAboutText: {
+        1: "I'm Adrian, a front-end developer with a background in Automation Engineering. While I have successfully navigated the intricate world of industry and technology, my true passion has always been for crafting user interfaces.",
+        2: "Eager for more challenges and yearning for the opportunity to specialize in UI, I decided to embark on a self-guided journey to master programming."
+    }
+}
+
+export const warningPage = {
+    warningText: {
+        1: "This website contains flashing animations and bright colors which may not be suitable for people with photosensitivity.",
+        2: "Remember to consult with your ripperdoc before entering this site, and proceed at your own discretion.",
+    },
+}
