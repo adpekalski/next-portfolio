6c3f099d103b1dcc7a3a1b9b298de1db1b85ae82
A. Pękalski <adpekalski@gmail.com>
2024-01-18 02:58:23 +0100

    Update Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,23 +5,33 @@ import Image from 'next/image';
 import test from '$/avatar.png';
 import './card.css';
 
-import { projectsData } from '@/data/ProjectsData';
+import { projectsData } from '@/data/WebsiteData';
 
 // import test from '../../../public/avatar.png';
 
 
 const Card = (props) => {
-    const { picture } = props;
+    const { whichOne } = props;
+
+    const projectLink = projectsData.linkToProject[whichOne];
 
     return (
         <div className='card'>
-            {/* <img src={test} /> */}
-            <Image
-                className='card-image'
-                alt='portfolio'
-                src={projectsData.projectImg[picture]}
-            />
-
+            {projectLink === "" || projectLink === undefined ?
+                <Image
+                    className='card-image'
+                    alt='portfolio'
+                    src={projectsData.projectImg[whichOne]}
+                />
+                :
+                <a href={projectLink} rel='noreferrer' target='_blank'>
+                    <Image
+                        className='card-image'
+                        alt='portfolio'
+                        src={projectsData.projectImg[whichOne]}
+                    />
+                </a>
+            }
         </div>
     );
 };
