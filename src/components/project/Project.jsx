6c99034a79fd437755ac1278f136fa646698a40b
A. Pękalski <adpekalski@gmail.com>
2024-01-18 02:58:41 +0100

    Update Project.jsx

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -3,38 +3,36 @@ import React from 'react';
 import { second } from '@/app/fonts';
 import './project.css';
 
-import { projectsData } from '@/data/ProjectsData';
-
+import { projectsData } from '@/data/WebsiteData';
+import Title from '../title/Title';
 import Card from '../card/Card';
 
 
 
 const Project = (props) => {
-    const { which, usedHeight, device } = props;
+    const { which, minHeight } = props;
 
     const [hidden, setHidden] = React.useState(false);
     let showMoreOrLess = hidden ? "Go back" : "Read more";
 
     let currentProject = projectsData.description[which];
-    let choosenCard = <Card picture={which}></Card>;
-    if (device === "mobile" || usedHeight < 800) {
+    let choosenCard = <Card whichOne={which}></Card>;
+    if (!minHeight) {
         currentProject = (
             <>
                 {hidden ? projectsData.description[which] + " " : projectsData.shortDescription[which] + " "}
 
-                <span className='expand-text'>{showMoreOrLess}</span>
+                <span className='green-text'>{showMoreOrLess}</span>
+
+
             </>
         );
-        choosenCard = hidden ? null : <Card picture={which}></Card>;
+        choosenCard = hidden ? null : <Card whichOne={which}></Card>;
 
     }
     return (
         <div className='project'>
-            <h1
-                className={second.className + ' project--name'}
-            >
-                {projectsData.projectName[which]}
-            </h1>
+            <Title>{projectsData.projectName[which]}</Title>
             {choosenCard}
             <p
                 className='project--desc'
@@ -43,8 +41,11 @@ const Project = (props) => {
                 }}
             >
                 {currentProject}
+
             </p>
-        </div>
+            {hidden ? <p style={{ textAlign: "center" }} className="green-text">More text icoming </p> : null
+}
+        </div >
     );
 };
 
