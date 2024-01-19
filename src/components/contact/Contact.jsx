d487cebe3c8d16b0a826bf0bd4cc2738c1a84441
A. Pękalski <adpekalski@gmail.com>
2024-01-19 12:30:35 +0100

    Enhance Contact form
    *Activated contact form
    *Added placeholders
    *Cleanup

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import emailjs from '@emailjs/browser';
-import { motion } from 'framer-motion';
 
 import './contact.css';
-import { second } from '@/app/fonts';
 
 import Title from '../title/Title';
 import Button from '../button/Button';
@@ -14,22 +12,17 @@ const Contact = () => {
     const [focus, setFocus] = React.useState(null);
     const [visible, setVisibility] = React.useState("non-visible");
 
-    // const { register } = React.useFormContext();
-
     const sendEmail = (e) => {
         e.preventDefault();
         setVisibility(true);
 
-
-        // emailjs.sendForm('service_ttmmwda', 'template_vwj89gi', form.current, 'Ae9_zqRZKzMwltn9s')
-        //     .then((result) => {
-        //         console.log(result.text);
-        //     }, (error) => {
-        //         console.log(error.text);
-        //     });
+        emailjs.sendForm('service_ttmmwda', 'template_vwj89gi', form.current, 'Ae9_zqRZKzMwltn9s')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
     };
-
-
 
     return (
 
@@ -46,7 +39,7 @@ const Contact = () => {
                                 onFocus={(e) => { setFocus(e.target.name) }}
                                 onBlur={() => { setFocus(null) }}
                                 required
-                            // placeholder='Your name'
+                                placeholder='Johnny Silverhand'
 
                             />
                         </div>
@@ -58,7 +51,7 @@ const Contact = () => {
                                 onFocus={(e) => { setFocus(e.target.name) }}
                                 onBlur={() => { setFocus(null) }}
                                 required
-                            // placeholder='Your e-mail address'
+                                placeholder='rockerboy@nightcity.com'
 
                             />
                         </div>
@@ -73,7 +66,7 @@ const Contact = () => {
                             onFocus={(e) => { setFocus(e.target.name) }}
                             onBlur={() => { setFocus(null) }}
                             required
-                        // placeholder='Please write your message here...'
+                            placeholder='I have a gig for you, merc.'
 
                         />
                     </div>
@@ -81,28 +74,12 @@ const Contact = () => {
                         <Button style="secondary" type="button" onClick={() => { textRef.current.value = "" }}>
                             Clear
                         </Button>
-                        {/* <button
-                            type="button"
-                            className='btn-clear secondary-btn btn'
-                            onClick={() => { textRef.current.value = "" }}
-                        >
-                            Clear
-                        </button> */}
-                        {/* <button
-                            type="submit"
-                            value="Send"
-                            className='btn-send primary-btn btn'
-                        >
-                            Submit
-                        </button> */}
                         <Button style="primary" type="submit" value="Send">
                             Submit
                         </Button>
-
                     </div>
                     <p className={"white-text " + `${visible}`}>Your message was sent succesfully</p>
                 </form>
-
 
             </div>
         </section>
